02af0c3304227eab7724d907dce3ccb0e4f3d8c2
Thiago Soares Laitz <t224898@g.unicamp.br>
2019-09-19 16:14:57 -0300

    Update interrupcao.c

--- interrupcao.c
+++ interrupcao.c
@@ -10,6 +10,7 @@
 
 extern void delay (short int q);//cria o protótipo da função externa delay 
 
+short int aux_PinChange;//auxiliar para cada pressionamento do botão contar apenas uma vez
 short int estado;
 short int tempo;
 short int sentido; //1 = horário // 0 = anti horário
@@ -17,24 +18,39 @@ unsigned char * sreg = (unsigned char *) 0x5f;//aponta para o registrador sreg
 unsigned char * eicra = (unsigned char *) 0x69;//aponta para o registrador eicra
 unsigned char * eimsk = (unsigned char *) 0x3d;//aponta para o registrador eimsk
 unsigned char * pcicr = (unsigned char *) 0x68;//aponta para o registrador pcicr
+unsigned char * ddrb = (unsigned char *) 0x24;//aponta para o registrador de direção B
+unsigned char * portb = (unsigned char *) 0x25;//aponta para o registrador PortB
+unsigned char * ddrd = (unsigned char *) 0x2A;//aponta para o registrador DDRD
+unsigned char * ddrc = (unsigned char *) 0x27;//aponta para o registrador DDRC
+unsigned char * pcmsk1 = (unsigned char *) 0x6C;//aponta para o registrador PCMSK1
 
 void setup(){
 	/*Realiza as configurações iniciais necessárias */
 	estado = 0;
 	tempo = 1000;
 	sentido = 1;
-	*pcicr = *pcicr | 0x01;//ativa pcie0 (responsável por pcint1)
+	aux_PinChange = 0;
+	*ddrd = *ddrd & 0x00;//defino a porta D como entrada
+	*ddrb = *ddrb | 0x07;//Defino os pinos PB0/PB1/PB2 como saída
+	*ddrc = *ddrc & 0x00;//Defino a porta C como entrada
+	*pcicr = *pcicr | 0x02;//ativa pcie1 (responsável por pcint8)
 	*eicra = *eicra | 0x03;//ISC01 e ISC00 = 1
 	*eimsk = *eimsk | 0x01;//int0 habilitado
 	*sreg = *sreg | 0x80;//ativo a interrupção global
+	*portb = *portb & 0x00;//saída inicial dos leds = 000
+	*pcmsk1 = *pcmsk1 | 0x01;//Ativa a interrupção pelo pino PCINT8
 }
 
-ISR(PCINT1_vect){//tratará a interrupção externa int1 - modo pin change
-	
-	if (sentido == 1){
-		sentido = 0;
+ISR(PCINT1_vect){//tratará a interrupção externa int8 - modo pin change
+	if (aux_PinChange == 0){
+		if (sentido == 1){
+			sentido = 0;
+			}else{
+			sentido = 1;
+		}
+		aux_PinChange = 1;
 	}else{
-		sentido = 1;
+		aux_PinChange = 0;
 	}
 	*sreg = *sreg | 0x80;//ativo a interrupção global
 }
@@ -51,15 +67,17 @@ ISR(INT0_vect){//tratará a interrupção INT0
 
 int main(void)
 {
+	setup();
     while (1) 
     {
+		//_delay_ms(1000);
 		delay(tempo);
 		if (sentido == 1){//Sentido horário
 			if (estado == 7){
 				estado = 0;
 			}
 			else{
-				estado++
+				estado++;
 			}
 		}else{//sentido anti horário
 			if (estado == 0){
@@ -71,22 +89,37 @@ int main(void)
 		//agora devemos alterar as cores do led RGB
 		if (estado == 0){
 			//rgb = 0 0 0
+			*portb = *portb & 0x00;
+			
 		}else if (estado == 1){
-			//rgb = 1 0 0
+			//rgb = 1 0 0 (PB0 PB1 PB2)
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x01;
+			
 		}else if (estado == 2){
 			// rgb = 0 1 0
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x02;
 		}else if (estado == 3){
 			//rgb = 0 0 1
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x04;
 		}else if (estado == 4){
 			//rgb = 1 1 0
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x03;
 		}else if (estado == 5){
 			//rgb = 0 1 1
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x06;
 		}else if (estado == 6){
 			//rgb = 1 0 1
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x05;
 		}else if (estado == 7){
 			//rgb = 1 1 1 
+			*portb = *portb & 0x00;
+			*portb = *portb | 0x07;
 		}
-		
     }
 }
-
